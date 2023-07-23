cd6208303102a098599009731eb3c716b80d948a
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-07-23 16:55:18 -0500

    func: added basic NavBar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Root from "./components/Routes/Root";
 import ErrorPage from "./components/Routes/ErrorPage";
 import NoteRoute from "./components/Routes/NoteRoute";
+import NavBar from "./components/Navigation/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Root />}></Route>
-      <Route path="/notes" element={<NoteRoute />}></Route>
-      <Route path="*" element={<ErrorPage />}></Route>
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Root />}></Route>
+        <Route path="/notes" element={<NoteRoute />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+    </>
   );
 }
 
